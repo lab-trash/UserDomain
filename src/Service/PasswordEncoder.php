cdffd9b70f304f23d60326478907b5fd60039af7
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2019-02-02 06:13:00 +0100

    feat(Registration): add EncodedPassword

--- src/Service/PasswordEncoder.php
+++ src/Service/PasswordEncoder.php
@@ -5,12 +5,15 @@ namespace BSP\Service;
 
 use BSP\Types\Contracts\IString;
 use BSP\Types\String\BasicString;
+use BSP\ValueObject\EncodedPassword;
 
 final class PasswordEncoder
 {
-    public static function hash(IString $plainPassword): IString
+    public static function hash(IString $plainPassword): EncodedPassword
     {
-        return new BasicString(password_hash($plainPassword(), PASSWORD_BCRYPT));
+        return new EncodedPassword(
+            new BasicString(password_hash($plainPassword(), PASSWORD_BCRYPT))
+        );
     }
 
     public static function verify(IString $plainPassword, IString $hash): bool
---
+++ tests/ValueObject/EncodedPasswordTest.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace BSP\Tests\ValueObject;
+
+use BSP\Types\String\BasicString;
+use BSP\ValueObject\EncodedPassword;
+use PHPUnit\Framework\TestCase;
+
+final class EncodedPasswordTest extends TestCase
+{
+    public function test_it_can_be_initialized(): void
+    {
+        $encodedPassword = new EncodedPassword(new BasicString('hash'));
+
+        $this->assertSame('hash', $encodedPassword());
+    }
+}
