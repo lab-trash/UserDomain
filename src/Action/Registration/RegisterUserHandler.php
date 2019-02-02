497682fedfd5d9587a620d9895d054482bcfb754
Nicolas Gomes de Oliveira <ngdo27@gmail.com>
2019-02-02 21:09:05 +0100

    feat(Registration): add commandHandler

---
+++ src/Action/Registration/RegisterUserHandler.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace BSP\Action\Registration;
+
+use BSP\CommandBus\Command;
+use BSP\CommandBus\CommandHandler;
+use BSP\Entity\User;
+use BSP\Port\IReadUser;
+use BSP\Port\IWriteUser;
+use BSP\Service\PasswordEncoder;
+use BSP\Types\Identity;
+use BSP\ValueObject\UserId;
+
+final class RegisterUserHandler implements CommandHandler
+{
+    private $iReadUser;
+    private $iWriteUser;
+
+    public function __construct(IReadUser $iReadUser, IWriteUser $iWriteUser)
+    {
+        $this->iReadUser = $iReadUser;
+        $this->iWriteUser = $iWriteUser;
+    }
+
+    /**
+     * @param RegisterUser $command
+     * @throws \Exception
+     */
+    public function handle(Command $command): void
+    {
+        if (!$this->iReadUser->isEmailAvailable($command->email())) {
+            throw new \Exception('user.email.not_available');
+        }
+
+        $user = new User(
+            new UserId(Identity::generate()),
+            $command->email(),
+            PasswordEncoder::hash($command->plainPassword())
+        );
+
+        $this->iWriteUser->add($user);
+    }
+}
---
+++ tests/Action/Registration/RegisterUserHandlerTest.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+namespace BSP\Tests\Action\Registration;
+
+use BSP\Action\Registration\RegisterUser;
+use BSP\Action\Registration\RegisterUserHandler;
+use BSP\Port\IReadUser;
+use BSP\Port\IWriteUser;
+use BSP\Types\Identity;
+use BSP\ValueObject\UserId;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+final class RegisterUserHandlerTest extends TestCase
+{
+    public function test_it_can_handle_register_user(): void
+    {
+        /** @var IReadUser|MockObject $iReadUser */
+        $iReadUser = $this->createMock(IReadUser::class);
+        /** @var IWriteUser|MockObject $iWriteUser */
+        $iWriteUser = $this->createMock(IWriteUser::class);
+
+        $iReadUser->method('isEmailAvailable')->willReturn(true);
+
+        $iReadUser
+            ->expects($this->once())
+            ->method('isEmailAvailable');
+
+        $iWriteUser
+            ->expects($this->once())
+            ->method('add');
+
+        $registerUserHandler = new RegisterUserHandler($iReadUser, $iWriteUser);
+
+        $registerUser = new RegisterUser('john.snow@winterfell.com', 'winterIsComing');
+
+        $registerUserHandler->handle($registerUser);
+    }
+}
